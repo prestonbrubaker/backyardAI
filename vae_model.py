d098a1ea0409762c5fa6c82477acb12d6c6627f3
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-05-26 14:43:58 -0500

    Update vae_model.py

--- vae_model.py
+++ vae_model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 
 # Number of Latent Variables
-global latent_dim
 latent_dim = 8  # You can change this value to control the number of latent variables
 
 class VAE(nn.Module):

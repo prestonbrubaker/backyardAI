f6234a8d56cc109a51f731dfd66a5b01f8ae047e
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-05-26 21:30:25 -0500

    Update train_vae.py

--- train_vae.py
+++ train_vae.py
@@ -28,7 +28,7 @@ transform = transforms.Compose([
 ])
 
 dataset = CustomImageDataset(image_folder='photos_processed', transform=transform)
-dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 vae = VAE().to(device)
